5f0ba6b1596d8d858d7111ca4602e30a747cbb2a
mojila <me.mojila@gmail.com>
2019-11-23 09:07:21 +0700

    listen perfomance API

--- action/component/memory-graph.js
+++ action/component/memory-graph.js
@@ -6,8 +6,8 @@ import last from 'lodash/last';
 import * as time from 'd3-time';
 
 const commonProperties = {
-    width: 500,
-    height: 200,
+    width: 600,
+    height: 300,
     animate: true,
     enableSlices: 'x',
   };
@@ -17,8 +17,10 @@ const MemoryGraph = () => {
 
     const [dataA, setDataA] = useState(range(10).map(i => ({
         x: time.timeSecond.offset(date, i),
-        y: 10 + Math.round(Math.random() * 50)
+        y: performance.memory.usedJSHeapSize / 1000
     })));
+    const [totalUsedMemory, setTotalUsedMemory] = useState(performance.memory.totalJSHeapSize / 1000);
+    const [limit, setLimit] = useState(performance.memory.jsHeapSizeLimit / 1000)
     
     useEffect(() => {
         let timer = setInterval(next, 1000);
@@ -30,9 +32,11 @@ const MemoryGraph = () => {
         let data = dataA.slice(1);
         data.push({
           x: time.timeSecond.offset(last(dataA).x, 1),
-          y: 10 + Math.round(Math.random() * 55)
+          y: performance.memory.usedJSHeapSize / 1000
+        //   y: 10 + Math.round(Math.random() * 55)
         });
 
+        setTotalUsedMemory(performance.memory.totalJSHeapSize / 1000);
         setDataA(data);
     };
 
@@ -40,10 +44,10 @@ const MemoryGraph = () => {
         <Pane padding={14} elevation={1}>
             <Line
                 {...commonProperties}
-                margin={{ top: 30, right: 50, bottom: 60, left: 50 }}
+                margin={{ top: 0, right: 30, bottom: 30, left: 50 }}
                 data={[{ id: 'Memory Used', data: dataA }]}
                 xScale={{ type: 'time', format: 'native' }}
-                yScale={{ type: 'linear', max: 100 }}
+                yScale={{ type: 'linear', max: totalUsedMemory }}
                 axisBottom={{
                     format: '%H:%M:%S',
                     legend: `Memory Used Graph`,
@@ -64,6 +68,11 @@ const MemoryGraph = () => {
                     grid: { line: { stroke: '#ddd', strokeDasharray: '1 2' } },
                 }}
             />
+            <Pane>
+                <Pane>Memory Used Active { last(dataA).y } Kilobytes</Pane>
+                <Pane>Memory Used Total { totalUsedMemory } Kilobytes</Pane>
+                <Pane>Memory Used Limit { limit } Kilobytes</Pane>
+            </Pane>
         </Pane>
     );
 }
