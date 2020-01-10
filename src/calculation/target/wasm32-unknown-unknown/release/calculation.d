c6c8379205449e5632604bca9306e4411e34ff3b
Mojila <me.mojila@gmail.com>
2020-01-10 23:26:56 +0700

    remove target

--- src/calculation/target/wasm32-unknown-unknown/release/calculation.d
+++
@@ -1,1 +0,0 @@
-/Users/mojila/Projects/memory-leak-detector/src/calculation/target/wasm32-unknown-unknown/release/calculation.wasm: /Users/mojila/Projects/memory-leak-detector/src/calculation/src/lib.rs
--- src/calculation/target/wasm32-unknown-unknown/release/deps/calculation.d
+++
@@ -1,5 +0,0 @@
-/Users/mojila/Projects/memory-leak-detector/src/calculation/target/wasm32-unknown-unknown/release/deps/calculation.wasm: src/lib.rs
-
-/Users/mojila/Projects/memory-leak-detector/src/calculation/target/wasm32-unknown-unknown/release/deps/calculation.d: src/lib.rs
-
-src/lib.rs:
