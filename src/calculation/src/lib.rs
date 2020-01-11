ba4cb0859cdbef735fe8e6f70220aec1f2c0c6d0
Mojila <me.mojila@gmail.com>
2020-01-11 16:48:36 +0700

    add outlier detection helper

--- src/calculation/src/lib.rs
+++ src/calculation/src/lib.rs
@@ -1,4 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    println!("{}", add(1,2));
+}
+
 #[no_mangle]
-pub fn sub(a: i32, b: i32) -> i32 {
-    a - b
+pub fn add(a: i32, b: i32) -> i32 {
+    return a + b
 }
