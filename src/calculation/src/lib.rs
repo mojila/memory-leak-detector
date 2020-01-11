7d358b8b9aaa0ff63b80fbde3c771eb7e102e0eb
Mojila <me.mojila@gmail.com>
2020-01-11 16:48:44 +0700

    add outlier detection helper

--- src/calculation/src/lib.rs
+++ src/calculation/src/lib.rs
@@ -1,10 +1,7 @@
-fn main() {
-    println!("Hello, world!");
-
-    println!("{}", add(1,2));
-}
+use average::Mean;
+use serde_json;
 
 #[no_mangle]
-pub fn add(a: i32, b: i32) -> i32 {
-    return a + b
+pub fn mean(sequence: ) {
+
 }
